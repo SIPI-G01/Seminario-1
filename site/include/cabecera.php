91389793c55888b281e1fd6d88677221aba72eeb
diegoritu <49616722+diegoritu@users.noreply.github.com>
2019-10-05 14:05:05 -0300

    Cambios en el estilo

--- site/include/cabecera.php
+++ site/include/cabecera.php
@@ -1,11 +1,10 @@
 <header id="masthead" class="masthead">
 <!--ACA VA EL HEADER-->
-    <ul>
-		<li><a class="active" href="#home">Inicio</a></li>
-		<li><a href="#news">FAQ</a></li>
-		<li><a href="#contact">Iniciar sesion</a></li>
-		<li><a href="#about">Registrarse</a></li>
-	</ul>
-
+<ul>
+        <li id=headerIzq ><a class="active" href="#inicio">Inicio</a></li>
+        <li id=headerIzq><a href="#faq">FAQ</a></li>
+        <li id=headerDer><a href="#registro">Registrarse</a></li>
+        <li id=headerDer><a href="#inicio_sesion">Iniciar sesion</a></li>
+    </ul>
 </header>
 
--- site/template/home/index.php
+++ site/template/home/index.php
@@ -1,10 +1,21 @@
-<div class="container">
-	<h1>Desafio Saludable</h1>
-	<h3>Seleccione su objetivo:</h3>
-	<input type=text list=objetivos >
-	<datalist id=objetivos >
-	   <option> Bajar de Peso
-	   <option> Subir de peso
-	   <option> Mantenerse en forma
-	</datalist>
+<div class= "container-fluid">
+
+<div id=nombrePag>
+    <h1>Desafio Saludable</h1>
+</div>
+<div id=selectObjetivos>
+    <h3>Seleccione su objetivo:</h3>
+</div>
+<div id= objetivosList>
+    <input type=text list=objetivos >
+    <datalist id=objetivos >
+       <option> Bajar de Peso
+       <option> Subir de peso
+       <option> Mantenerse en forma
+    </datalist>
+</div>
+<div id=botonBuscar>
+    <button id=buscar>Buscar</button>
+</div>
+
 </div>
