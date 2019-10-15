35e59b8d8207e8dbce450fc7b25e64ae18bec53c
Ezem98 <56078650+Ezem98@users.noreply.github.com>
2019-10-15 20:55:51 -0300

    cambios en base de datos

---
+++ base/8. 2019-10-15 Agregar datos a tabla publicacion_comentario.sql
@@ -0,0 +1,3 @@
+INSERT INTO `publicacion_comentario` (id_publicacion,id_usuario,texto,fecha) VALUES (1,1,'Esto es un comentario de prueba','2019-10-15');
+
+INSERT INTO `publicacion_comentario` (id_publicacion,id_usuario,texto,fecha) VALUES (1,2,'Otro comentario de prueba´','2019-10-15');
