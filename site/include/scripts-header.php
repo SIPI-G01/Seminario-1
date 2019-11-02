0c695242bfc3185785e2603643b480d5cb815ee7
Agustin Stanoff <agustinstanoff98@gmail.com>
2019-11-02 15:32:33 -0300

    Cambios en el footer

--- site/include/scripts-header.php
+++ site/include/scripts-header.php
@@ -15,6 +15,7 @@
 		<script src="https://kit.fontawesome.com/76596c4599.js" crossorigin="anonymous"></script>
 
 		<!-- Summernote -->
+		
   <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
   <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script> 
   <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script> 
