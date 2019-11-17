e2c5e686f4f8305c48056320bed58f23cf190427
Axel <axelr@live.com.ar>
2019-11-17 00:37:53 -0300

    Arreglos en cabecera.

--- site/include/cabecera.php
+++ site/include/cabecera.php
@@ -8,9 +8,9 @@
 		
     <ul style="position: relative;">
         <li id="headerIzq" ><a class="active" href="/home">Inicio</a></li>
-        <li id="headerIzq"><a href="#faq">FAQ</a></li>
+        <li id="headerIzq"><a href="/faq">FAQ</a></li>
 		<li id="headerIzq" class="centrar">
-			<a href="/home" style="padding-top: 7px; padding-bottom: 0px;"><img class="imagen-inicio" src="/site/images/logo.png"> Vita</a>
+			<a href="/home" style="padding-top: 7px; padding-bottom: 0px; font-size: 25px;"><img class="imagen-inicio" src="/site/images/logo.png"> Vita</a>
 		</li>
 		<?php if(Utiles::obtenerUsuarioLogueado() == null){ ?>
 			<li id="headerDerLogIn">
@@ -56,7 +56,10 @@
 </header>
 
 <script>
-    function registrarse(){}
+    function registrarse()
+	{
+		window.location = '/registrarse';
+	}
 	
 	function cerrarSesion() {
 
